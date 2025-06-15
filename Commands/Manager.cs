467ff88aea82490393534e3ed1ab682b4de00af2
YellowMacaroni <mattctaylor452@gmail.com>
2025-06-15 22:24:06 +0100

    Added staff command support

--- Commands/Manager.cs
+++ Commands/Manager.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Whispbot.Commands.General;
+using Whispbot.Commands.Staff;
+using YellowMacaroni.Discord.Cache;
 using YellowMacaroni.Discord.Core;
+using YellowMacaroni.Discord.Extentions;
 using YellowMacaroni.Discord.Sharding;
 
 namespace Whispbot.Commands
 {
     public class CommandManager
     {
-        private List<Command> Commands = [];
+        private readonly List<Command> _commands = [];
+        private readonly List<Command> _staffCommands = [];
 
         public CommandManager()
         {
@@ -20,6 +24,9 @@ namespace Whispbot.Commands
 
             RegisterCommand(new Ping());
             RegisterCommand(new About());
+            RegisterCommand(new Support());
+
+            RegisterStaffCommand(new Test());
 
             #endregion
 
@@ -27,8 +34,14 @@ namespace Whispbot.Commands
 
         public void RegisterCommand(Command command)
         {
-            if (Commands.Any(c => c.Name == command.Name)) return;
-            Commands.Add(command);
+            if (_commands.Any(c => c.Name == command.Name)) return;
+            _commands.Add(command);
+        }
+
+        public void RegisterStaffCommand(Command command)
+        {
+            if (_staffCommands.Any(c => c.Name == command.Name)) return;
+            _staffCommands.Add(command);
         }
 
         public void HandleMessage(Client client, Message message)
@@ -36,16 +49,33 @@ namespace Whispbot.Commands
             string prefix = Config.IsDev ? "a!" : "b!";
             string mention = $"<@{client.readyData?.user.id}>";
 
+            string staffPrefix = Config.IsDev ? ">>>" : ">>";
+
             if (message.content.StartsWith(mention)) prefix = mention;
 
-            if (!message.content.StartsWith(prefix)) return;
+            if (message.content.StartsWith(prefix, StringComparison.CurrentCultureIgnoreCase))
+            {
+                List<string> args = [.. message.content[prefix.Length..].Split(' ', StringSplitOptions.RemoveEmptyEntries)];
+                string commandName = args[0].ToLowerInvariant(); args.RemoveAt(0);
 
-            List<string> args = [.. message.content[prefix.Length..].Split(' ', StringSplitOptions.RemoveEmptyEntries)];
-            string commandName = args[0].ToLowerInvariant(); args.RemoveAt(0);
+                Command? command = _commands.Find(c => c.Aliases.Contains(commandName));
 
-            Command? command = Commands.Find(c => c.Aliases.Contains(commandName));
+                command?.ExecuteAsync(new CommandContext(client, message, args));
+            }
+            else if 
+            (
+                message.content.StartsWith(staffPrefix, StringComparison.CurrentCultureIgnoreCase)
+                //                          |   Support Server  |             ->          |     Member    |               ->            |  Has Staff Role?  |
+                && (DiscordCache.Guilds.Get("1096509172784300174").WaitFor()?.members.Get(message.author.id).WaitFor()?.roles?.Contains("1256333207599841435") ?? false)
+            )
+            {
+                List<string> args = [.. message.content[staffPrefix.Length..].Split(' ', StringSplitOptions.RemoveEmptyEntries)];
+                string commandName = args[0].ToLowerInvariant(); args.RemoveAt(0);
 
-            command?.ExecuteAsync(new CommandContext(client, message, args));
+                Command? command = _staffCommands.Find(c => c.Aliases.Contains(commandName));
+
+                command?.ExecuteAsync(new CommandContext(client, message, args));
+            }
         }
 
         public void Attach(Client client)
---
+++ Commands/Staff/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whispbot.Commands.Staff
+{
+    public class Test: Command
+    {
+        public override string Name => "Test";
+        public override string Description => "A test command for staff.";
+        public override List<string> Aliases => ["test"];
+        public override List<string> Usage => [];
+        public override async Task ExecuteAsync(CommandContext ctx)
+        {
+            await ctx.Reply("Hello world!");
+        }
+    }
+}
