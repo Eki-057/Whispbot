5258862258ce1bea22d64296b0b4fc476b369765
YellowMacaroni <mattctaylor452@gmail.com>
2025-08-16 21:35:59 +0100

    Added clusters

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using Npgsql;
 using Whispbot.Extensions;
 using Serilog;
 using Whispbot.API;
+using YellowMacaroni.Discord.Extentions;
 
 Logger.Initialize();
 
@@ -29,17 +30,117 @@ if (token is null)
 _ = Task.Run(Redis.Init);
 _ = Task.Run(Postgres.Init);
 _ = Task.Run(SentryConnection.Init);
+_ = Task.Run(Tools.Strings.GetEmojis);
+_ = Task.Run(Tools.Strings.GetLanguages);
 
-Thread APIThread = new(new ThreadStart(WhispbotAPI.Start))
+Thread APIThread = new(new ThreadStart(() =>
+{
+    if (Config.IsDev)
+    {
+        while (Config.cluster == -1) Thread.Sleep(100);
+        if (Config.cluster != 0)
+        {
+            Log.Information("Skipping API startup in non-leader cluster.");
+            return;
+        }
+    }
+
+    WhispbotAPI.Start();
+}))
 {
     Name = "Whispbot API",
     IsBackground = true
 };
 APIThread.Start();
 
+int clusters = 1;
+if (!dev)
+{
+    clusters = await Tools.Railway.getReplicaCount();
+}
+
+Log.Information($"Starting cluster. Total clusters: {clusters}.");
+
+Config.replica = new(
+    Config.deploymentId,
+    Config.replicaId,
+    clusters
+);
+await Config.replica.Start();
+
+while (!Config.replica.active) Thread.Sleep(100);
+
+ISubscriber? pubSub = null;
+
+while (pubSub is null)
+{
+    pubSub = Redis.GetSubscriber();
+    if (pubSub is null) Thread.Sleep(500);
+}
+
+void register (RedisValue message)
+{
+    string[] parts = message.ToString().Split(':');
+    string deploymentId = parts[0];
+    string replicaId = parts[1];
+
+    if (deploymentId != Config.deploymentId) return;
+
+    if (!Config.replicas.Contains(replicaId)) Config.replicas.Add(replicaId);
+
+    pubSub.Publish($"{Config.deploymentId}-replicas", JsonConvert.SerializeObject(Config.replicas));
+}
+
+if (Config.replica.IsLeader)
+{
+    Config.cluster = 0;
+    Config.replicas.Add(Config.replicaId);
+
+    pubSub.Subscribe("register", (channel, message) => { register(message); });
+
+    _ = DiscordLogger.Log($"[{{dt}}] {{emoji.loading}} Re-clustering {clusters} clusters...".Process());
+}
+else
+{
+    pubSub.Subscribe($"{Config.deploymentId}-replicas", (channel, message) =>
+    {
+        Config.replicas = JsonConvert.DeserializeObject<List<string>>(message.ToString()) ?? [];
+        Config.cluster = Config.replicas.IndexOf(Config.replicaId);
+    });
+}
+
+Config.replica.OnElected += (_, _) =>
+{
+    pubSub.Subscribe("register", (channel, message) => { register(message); });
+};
+
+Config.replica.OnLostLeadership += (_, _) =>
+{
+    pubSub.Unsubscribe("register");
+};
+
+while (Config.cluster == -1)
+{
+    pubSub.Publish("register", $"{Config.deploymentId}:{Config.replicaId}");
+    Thread.Sleep(200);
+}
+
+Log.Information($"Cluster {Config.cluster} established and waiting for start signal.");
+
+string? shardsEnv = Config.IsDev ? null : Environment.GetEnvironmentVariable("SHARDS");
+int? shards = shardsEnv is null ? null : int.Parse(shardsEnv);
+
 ShardingManager sharding = new(
     token,
-    Intents.GuildMessages | Intents.MessageContent | Intents.Guilds);
+    Intents.GuildMessages | Intents.MessageContent | Intents.Guilds,
+    shards
+);
+
+int shardCount = sharding.shards.Count;
+int clusterStart = (int)(Config.cluster * ((float)shardCount/(float)clusters));
+int clusterEnd = (int)((Config.cluster + 1) * ((float)shardCount/(float)clusters));
+
+sharding.shards = [..sharding.shards.Skip(clusterStart).Take(clusterEnd - clusterStart)];
 
 CommandManager commands = new();
 commands.Attach(sharding);
@@ -48,12 +149,72 @@ foreach (Shard shard in sharding.shards)
 {
     shard.client.On("READY", (client, obj) =>
     {
-        Log.Information("Shard {ShardId} online!", shard.id);
+        _ = DiscordLogger.Log($"[{{dt}}] {{emoji.clockedin}} Shard `{shard.id}` of cluster `{Config.cluster}` is ready.".Process());
+        Log.Information($"Cluster {Config.cluster.ToString().PadLeft((Config.replicas.Count - 1).ToString().Length, '0')}, shard {shard.id.ToString().PadLeft((shardCount - 1).ToString().Length, '0')} online!");
     });
+
+    shard.client.Connecting += (client, args) =>
+    {
+        _ = DiscordLogger.Log($"[{{dt}}] {{emoji.break}} Shard `{shard.id}` of cluster `{Config.cluster}` is starting...".Process());
+        Log.Information($"Cluster {Config.cluster.ToString().PadLeft((Config.replicas.Count - 1).ToString().Length, '0')}, shard {shard.id.ToString().PadLeft((shardCount - 1).ToString().Length, '0')} starting.");
+    };
 }
 
-Log.Information("Starting...");
+bool start = Config.cluster == 0;
+bool isLastCluster = false;
+if (!start)
+{
+    pubSub.Subscribe($"start", (channel, message) =>
+    {
+        string[] parts = message.ToString().Split(':');
+        string replica = parts[0];
+        if (replica == Config.replicaId)
+        {
+            start = true;
+            isLastCluster = parts[1] == "1";
+        }
+    });
+    
+}
+
+double lastRequestedStart = 0;
+while (!start) 
+{ 
+    Thread.Sleep(100);
+    if (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - lastRequestedStart > 5000)
+    {
+        lastRequestedStart = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        pubSub.Publish($"{Config.deploymentId}-can-start", Config.replicaId);
+    }
+}
+
+Log.Information($"Cluster {Config.cluster} starting shards {clusterStart} to {clusterEnd - 1}.");
+_ = DiscordLogger.Log($"[{{dt}}] {{emoji.break}} Cluster `{Config.cluster}` is starting...".Process());
+
+_ = Task.Run(() =>
+{
+    string? nextReplica = Config.replicas.ElementAtOrDefault(Config.cluster + 1);
+    while (!sharding.shards.All(s => s.client.ready)) Thread.Sleep(100);
+    _ = DiscordLogger.Log($"[{{dt}}] {{emoji.clockedin}} Cluster `{Config.cluster}` is ready.".Process());
+    Thread.Sleep(5000); // Make sure no overlap with other clusters
+    if (nextReplica is not null && !isLastCluster)
+    {
+        pubSub.Publish("start", $"{nextReplica}:0");
+        Log.Information($"All cluster {Config.cluster} shards started, sending command to start cluster {Config.cluster + 1}.");
+    }
+    else
+    {
+        Log.Information($"All cluster {Config.cluster} shards started, all cluters started.");
+        pubSub.Subscribe($"{Config.deploymentId}-can-start", (channel, message) =>
+        {
+            pubSub.Publish("start", $"{message.ToString()}:1");
+            pubSub.Unsubscribe($"{Config.deploymentId}-can-start");
+            Log.Information($"Cluster {Config.replicas.IndexOf(message.ToString())} taking last cluster from {Config.cluster}.");
+        });
+    }
+});
+
 sharding.Start();
 sharding.Hold();
-Log.Information("Stopping...");
+Log.Information("Stopping bot...");
 Logger.Shutdown();
---
+++ Replica.cs
@@ -0,0 +1,240 @@
+﻿using Serilog;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Whispbot.Databases;
+
+namespace Whispbot
+{
+    public class Replica
+    {
+        private readonly ISubscriber _subscriber;
+        private readonly string _id;
+        private readonly string _deployment;
+
+        private int _currentTerm = 0;
+        private string? _votedFor = null;
+        private bool _isLeader = false;
+        private double _lastHeartbeat = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        private CancellationTokenSource? _cancellationTokenSource = null;
+        private int _failedElections = 0;
+        private bool _currentlyRunning = false;
+
+        private readonly Random _random = new();
+
+        public readonly int total_replicas;
+        public bool active = false;
+        public bool IsLeader => _isLeader;
+
+        // CONFIG
+        public readonly int heartbeatInterval = Config.IsDev ? 1000 : 100;
+        public readonly int electionTimeout = Config.IsDev ? 5000 : 250;
+        public readonly int electionDuration = Config.IsDev ? 1000 : 200;
+        public readonly int timeoutMin = Config.IsDev ? 500 : 100;
+        public readonly int timeoutMax = Config.IsDev ? 2000 : 300;
+
+        public event EventHandler? OnElected;
+        public event EventHandler? OnLostLeadership;
+
+
+        public Replica(string deployment, string replica, int total_replicas)
+        {
+            _id = replica;
+            _deployment = deployment;
+
+            ISubscriber? sub = null;
+            int count = 0;
+            while (sub is null)
+            {
+                sub = Redis.GetSubscriber(); // Wait for redis subsub to be ready
+                if (sub is null)
+                {
+                    count++;
+                    if (count >= 10)
+                    {
+                        throw new Exception("Failed to connect to redis for cluster manager");
+                    }
+                    Thread.Sleep(100 * count * count);
+                }
+            }
+            _subscriber = sub;
+
+            this.total_replicas = total_replicas;
+        }
+
+        private string LogPrefix => $"[{_currentTerm.ToString().PadLeft(3, '0')}]";
+
+        public async Task Start()
+        {
+            await _subscriber.SubscribeAsync("request_vote", (channel, message) => RecievedElection(message));
+            await _subscriber.SubscribeAsync("heartbeat", (channel,message) => RecievedHeartbeat(message));
+
+            _ = Task.Run(ElectionTimeoutLoop);
+
+            Log.Information($"{LogPrefix} Ready.");
+        }
+
+        private async Task ElectionTimeoutLoop()
+        {
+            while (true)
+            {
+                int timeout = _random.Next(timeoutMin, timeoutMax);
+                Thread.Sleep(timeout);
+
+                if (!_isLeader && (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _lastHeartbeat) > electionTimeout && _votedFor is null) // Is not the leader and hasnt had heartbeat in 500ms
+                {
+                    Log.Information($"{LogPrefix} No heartbeat in {DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _lastHeartbeat}ms, starting election.");
+                    await StartElection();
+                }
+            }
+        }
+
+        private void Elected()
+        {
+            _isLeader = true;
+            _currentlyRunning = false;
+            _subscriber.UnsubscribeAsync("vote");
+            _failedElections = 0;
+            OnElected?.Invoke(this, EventArgs.Empty);
+            Log.Information($"{LogPrefix} Elected as new leader.");
+            StartHeartbeat();
+        }
+
+        private async Task StartElection()
+        {
+            _currentTerm++;
+            int majority = (total_replicas / 2) + 1 - (_failedElections);
+            int votes = 1; // Vote for self
+
+            if (majority <= 1)
+            {
+                Log.Information($"{LogPrefix} Majority is {majority}, no election needed.");
+                Elected();
+                return;
+            }
+            
+            _currentlyRunning = true;
+
+            await _subscriber.SubscribeAsync("vote", (channel, message) =>
+            {
+                string[] parts = message.ToString().Split(':');
+                string deployment = parts[0];
+                int term = int.Parse(parts[1]);
+                string replica = parts[2];
+
+                if (deployment != _deployment) return; // Ignore messages from other deployments
+
+                if (term == _currentTerm && replica == _id)
+                {
+                    votes++;
+
+                    if (votes >= majority && !_isLeader && _currentlyRunning)
+                    {
+                        Elected();
+                    }
+                }
+            });
+
+            await _subscriber.PublishAsync("request_vote", $"{_deployment}:{_currentTerm}:{_id}");
+
+            Thread.Sleep(electionTimeout);
+
+            if (!_isLeader && _currentlyRunning)
+            {
+                _failedElections++;
+                _currentlyRunning = false;
+                _lastHeartbeat = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                await _subscriber.UnsubscribeAsync("vote");
+                Log.Warning($"{LogPrefix} Not elected as new leader, failure {_failedElections}.");
+            }
+        }
+
+        private void StartHeartbeat()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = _cancellationTokenSource.Token;
+
+            _ = Task.Run(async () =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    await _subscriber.PublishAsync("heartbeat", $"{_deployment}:{_id}:{_currentTerm}");
+                    Thread.Sleep(heartbeatInterval);
+                }
+            });
+        }
+
+        private void RecievedHeartbeat(RedisValue message)
+        {
+            string[] parts = message.ToString().Split(':');
+            string deployment = parts[0];
+            string replica = parts[1];
+            int term = int.Parse(parts[2]);
+
+            if (deployment != _deployment) return; // Ignore messages from other deployments
+
+            active = true;
+
+            if (replica == _id) return; // Ignore own heartbeat
+
+            if (term > _currentTerm)
+            {
+                _currentTerm = term;
+                _isLeader = false;
+                _cancellationTokenSource?.Cancel();
+                _cancellationTokenSource = null;
+                _votedFor = null;
+                _lastHeartbeat = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                _failedElections = 0; // Reset failed elections
+                OnLostLeadership?.Invoke(this, EventArgs.Empty);
+                Log.Information($"{LogPrefix} Accepted {replica} as leader.");
+            } 
+            else if (term == _currentTerm)
+            {
+                if (_isLeader)
+                {
+                    _isLeader = false;
+                    _cancellationTokenSource?.Cancel();
+                    _cancellationTokenSource = null;
+                    OnLostLeadership?.Invoke(this, EventArgs.Empty);
+                    Log.Error($"{LogPrefix} Lost leadership to {replica}.");
+                }
+                if (_failedElections > 0 || _currentlyRunning)
+                {
+                    Log.Error($"{LogPrefix} Lost election to {replica}.");
+                }
+                _failedElections = 0; // Reset failed elections
+                _currentlyRunning = false; // Reset running state
+                _votedFor = null; // Reset vote
+                _lastHeartbeat = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            }
+        }
+
+        private void RecievedElection(RedisValue message)
+        {
+            string[] parts = message.ToString().Split(':');
+            string deployment = parts[0];
+            int term = int.Parse(parts[1]);
+            string replica = parts[2];
+
+            if (deployment != _deployment) return; // Ignore messages from other deployments
+
+            if (term > _currentTerm && replica != _id && !_currentlyRunning) // vote
+            {
+                _currentTerm = term;
+                _votedFor = replica;
+                _isLeader = false;
+                _cancellationTokenSource?.Cancel();
+                _cancellationTokenSource = null;
+
+                _subscriber.PublishAsync("vote", $"{_deployment}:{_currentTerm}:{replica}");
+
+                Log.Information($"{LogPrefix} Accepted {replica} as leader.");
+            } // otherwise no vote
+        }
+    }
+}
